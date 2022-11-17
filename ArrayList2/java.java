59f5658608f4b40757fc1e26a35bd1e24026fec3
asif-bhai-xoss <78955762+asif-bhai-xoss@users.noreply.github.com>
2022-11-17 15:21:39 +0600

    ArrayList

--- ArrayList2/java.java
+++ ArrayList2/java.java
@@ -6,6 +6,7 @@
 package ArrayList2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
@@ -18,12 +19,25 @@ public class java {
         ArrayList<Integer> numbers3 = new ArrayList<Integer>();
         
         numbers1.add(10);
-        numbers1.add(20);
         numbers1.add(30);
+        numbers1.add(20);
+        System.out.println("numbers1 = " + numbers1);
         
         numbers2.add(1);
         numbers2.add(2);
         numbers2.add(3);
+        System.out.println("numbers2 = " + numbers2);
+        
+        numbers3.addAll(numbers1);
+         System.out.println("numbers3 = " + numbers3);
+        
+        boolean isEqual = numbers1.equals(numbers2);
+        System.out.println(isEqual);
+        
+        Collections.sort(numbers1);
+        Collections.sort(numbers1,Collections.reverseOrder());
+        
+        
         
     }
 }
